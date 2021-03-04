80a9c8ddb7de8bf6ec3a14aade45e92b829fba7d
Grace Cramer <grace_cramer1@baylor.edu>
2021-03-03 22:04:10 -0600

    gang

---
+++ file4.py
@@ -0,0 +1,56 @@
+import json
+
+infile = open("US_fires_9_14.json", "r")
+outfile = open("readable_fires_9_14.json", "w")
+
+# json.load converted data into a formate python
+# can work with (a giant dictionary)
+
+fire_data = json.load(infile)
+
+json.dump(fire_data, outfile, indent=4)
+
+list_of_fires = fire_data
+
+brights = []
+lons = []
+lats = []
+
+for fire in list_of_fires:
+    brightness = fire["brightness"]
+    lon = fire["longitude"]
+    lat = fire["latitude"]
+    if brightness > 450:
+        brights.append(brightness)
+        lons.append(lon)
+        lats.append(lat)
+
+# print(brights[:10])
+# print(lons[:10])
+# print(lats[:10])
+
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+data = [
+    {
+        "type": "scattergeo",
+        "lon": lons,
+        "lat": lats,
+        "marker": {
+            "size": [0.02 * brightness for brightness in brights],
+            "color": brights,
+            "colorscale": "Viridis",
+            "reversescale": True,
+            "colorbar": {"title": "Brightness"},
+        },
+    }
+]
+Scattergeo(lon=lons, lat=lats)  # taking scatterplots and putting on a map
+
+my_layout = Layout(title="Prominent US Fires")  # give layout for ^
+
+fig = {"data": data, "layout": my_layout}
+
+offline.plot(fig, filename="us_fires.html")
